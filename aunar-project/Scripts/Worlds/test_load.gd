54940c50eb81e9dea24bf57ea0b4dc7c466f2052
Meldarse <meldarse@gmail.com>
2024-09-26 09:01:21 -0500

    automatización de creación

--- aunar-project/Scripts/Worlds/test_load.gd
+++ aunar-project/Scripts/Worlds/test_load.gd
@@ -5,40 +5,64 @@ var path_ost: String = "res://Assets/OST/"
 
 
 func _ready() -> void:
-	dir_contents(path_ost)
-
-	#audio.stream.set_stream_count(5)
-	#print(audio.stream.get_stream_count())
+	dir_contents()
+	get_tree().quit()
 
 
-
-func dir_contents(path: String) -> void:
-	var dir = DirAccess.open(path)
+func dir_contents() -> void:
+	var dir = DirAccess.open(path_ost)
 	if dir:
 		dir.list_dir_begin()
 		var file_name = dir.get_next()
 		while file_name != "":
-			create_audio_node(file_name.substr(0, 1), file_name.substr(2))
-			#print("Nombre del archivo/subdirectorio: ", file_name)
-			#print("Es un directorio: ", dir.current_is_dir())
+			create_audio_node(file_name)
 			file_name = dir.get_next()
 		dir.list_dir_end()
 	else:
 		print("Error al abrir el directorio")
 
-func create_audio_node(type: String, name_node: String) -> void:
+
+func create_audio_node(file_name: String) -> void:
 	var node_playlist = AudioStreamPlayer.new()
 	var stream
 	var scene = PackedScene.new()
-	var save_path = "res://Aunar_template/" + name_node + ".tscn"
-	match type:
+	var save_path = "res://Aunar_template/" + file_name.substr(2) + ".tscn"
+	var sound_path = path_ost + file_name
+
+	match file_name.substr(0,1):
 		"P":
 			stream = AudioStreamPlaylist.new()
 		"I":
 			stream = AudioStreamInteractive.new()
 		"S":
 			stream = AudioStreamSynchronized.new()
-	node_playlist.set_name(name_node)
+
+	node_playlist.set_name(file_name.substr(2))
 	node_playlist.set_stream(stream)
+
+	var dir = DirAccess.open(sound_path)
+	if dir:
+		dir.list_dir_begin()
+		var sound_name = dir.get_next()
+		var number_channels: int = 0
+		while sound_name != "":
+			if sound_name.ends_with(".wav") or file_name.ends_with(".mp3") or file_name.ends_with(".ogg"):
+					number_channels += 1
+					match file_name.substr(0,1):
+						"P":
+							node_playlist.stream.set_stream_count(number_channels)
+							node_playlist.stream.set_list_stream(number_channels - 1, load(sound_path +"/"+ sound_name))
+						"I":
+							node_playlist.stream.set_clip_count(number_channels)
+							node_playlist.stream.set_clip_stream(number_channels - 1, load(sound_path +"/"+ sound_name))
+							node_playlist.stream.set_clip_name(number_channels - 1, sound_name.get_basename())
+						"S":
+							node_playlist.stream.set_stream_count(number_channels)
+							node_playlist.stream.set_sync_stream(number_channels - 1, load(sound_path +"/"+ sound_name))
+			sound_name = dir.get_next()
+		dir.list_dir_end()
+	else:
+		print("Error al abrir archivo de audio")
+
 	scene.pack(node_playlist)
 	ResourceSaver.save(scene, save_path)
--- aunar-project/Scripts/Worlds/test_save_clip.gd
+++ aunar-project/Scripts/Worlds/test_save_clip.gd
@@ -1,8 +1,17 @@
 extends Control
 
-@onready var audio: AudioStreamPlayer = $AudioStreamPlayer
 
 
 func _ready() -> void:
-	audio.set_stream(load("res://Assets/OST/S_APZX_CyberMower/01_Kick.wav"))
-	audio.play()
+	pass
+	#var node_playlist = AudioStreamPlayer.new()
+	#var stream = AudioStreamInteractive.new()
+	#var scene = PackedScene.new()
+	#var save_path = "res://Aunar_template/Audio.tscn"
+	##node_playlist.set_name("Audio")
+	#node_playlist.set_stream(stream)
+	#node_playlist.stream.set_clip_count(5)
+	#node_playlist.stream.set_clip_name(0, "Audio_0")
+	#node_playlist.stream.set_clip_stream(0, load("res://Assets/OST/S_APZX_CyberMower/01_Kick.wav"))
+	#scene.pack(node_playlist)
+	#ResourceSaver.save(scene, save_path)
